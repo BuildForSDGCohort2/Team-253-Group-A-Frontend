6d05ce41b1005a407cd6adc1be718e1f70fe0e06
khammami <khammami@gmail.com>
2020-09-09 17:58:32 +0100

    Fix null in get intials

--- src/components/AccountMenu.js
+++ src/components/AccountMenu.js
@@ -30,12 +30,16 @@ export default function AccountMenu() {
   };
 
   const getInitials = (string) => {
-    var names = string.split(' '),
-        initials = names[0].substring(0, 1).toUpperCase();
+    var initials = "";
+    if (string != null) {
+      var names = string.split(' ');
+      initials = names[0].substring(0, 1).toUpperCase();
     
-    if (names.length > 1) {
-        initials += names[names.length - 1].substring(0, 1).toUpperCase();
+      if (names.length > 1) {
+          initials += names[names.length - 1].substring(0, 1).toUpperCase();
+      }
     }
+    
     return initials;
   };
 
