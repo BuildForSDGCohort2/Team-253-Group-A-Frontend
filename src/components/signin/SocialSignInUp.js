6826f7a4ce4026acee8a1323378ee4f37dc6c81c
khammami <khammami@gmail.com>
2020-09-09 10:42:43 +0100

    Handle 3rd party Auth errors

--- src/components/signin/SocialSignInUp.js
+++ src/components/signin/SocialSignInUp.js
@@ -5,6 +5,10 @@ import { useAuth } from 'reactfire';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,19 +29,63 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SocialSignInUp(props) {
   const classes = useStyles();
-
   const auth = useAuth();
+  const [open, setOpen] = React.useState(false);
+  const [alertSeverity, setAlertSeverity] = React.useState("");
+  const [alertMessage, setAlertMessage] = React.useState("");
 
   const signInGoogle = async () => {
-    await auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    await auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+                .then((result)=>{
+                  handleAuthSuccess(result);
+                }).catch((error)=>{
+                  handleAuthError(error);
+                });
   };
 
   const signInTwitter = async () => {
-    await auth.signInWithPopup(new firebase.auth.TwitterAuthProvider());
+    await auth.signInWithPopup(new firebase.auth.TwitterAuthProvider())
+                .then((result)=>{
+                  handleAuthSuccess(result);
+                }).catch((error)=>{
+                  handleAuthError(error);
+                });
   };
 
   const signInFacebook = async () => {
-    await auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+    await auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+                .then((result)=>{
+                  handleAuthSuccess(result);
+                }).catch((error)=>{
+                  handleAuthError(error);
+                });
+  };
+
+  const handleAuthError = (error) => {
+    if(error.code === "auth/account-exists-with-different-credential"){
+      setAlertMessage(error.message);
+      setAlertSeverity("error");
+      setOpen(true);
+    }
+  };
+
+  const handleAuthSuccess = (data) => {
+    if(data.additionalUserInfo.isNewUser){
+      setAlertMessage("Welcome to CleanOut! you have been signed up successfully as "+data.additionalUserInfo.profile.email);
+    } else {
+      setAlertMessage("Welcome back "+data.additionalUserInfo.profile.name+"!");
+    }
+
+    setAlertSeverity("success");
+    setOpen(true);
+  };
+
+  const handleCloseAlert = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
   };
 
   return (
@@ -51,6 +99,17 @@ export default function SocialSignInUp(props) {
         <Button className={classes.socialButton} size="large" variant="outlined" color="secondary" onClick={signInFacebook}>
         sign {props.signText} with Facebook
         </Button>
+        <Snackbar  open={open} autoHideDuration={6000} onClose={handleCloseAlert}>
+        <Alert severity={alertSeverity} action={
+          <React.Fragment>
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleCloseAlert}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }>
+          {alertMessage}
+        </Alert>
+      </Snackbar>
     </Paper>
   );
 }
