0a4e2eb777cdb320e2952cb70794d4977e5ced0d
khammami <khammami@gmail.com>
2020-09-12 10:02:56 +0100

    Clean unused Firebase SDKs

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,47 +1,41 @@
-import React from 'react';
-import {
-  Switch,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 import {
   useFirebaseApp,
   preloadUser,
   preloadAuth,
   preloadFirestore,
-  preloadDatabase,
   preloadStorage,
   SuspenseWithPerf,
   preloadAnalytics,
-} from 'reactfire';
+} from "reactfire";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
+import Loading from "../Loading";
 
-import Loading from '../Loading';
-
-const Header = React.lazy(() => import('../Header'));
-const Footer = React.lazy(() => import('../Footer'));
-const Home = React.lazy(() => import('../Home'));
-const SignIn = React.lazy(() => import('../signin/SignIn'));
-const Register = React.lazy(() => import('../signup/Register'));
-const About = React.lazy(() => import('../About'));
-const GenericNotFound = React.lazy(() => import('../404'));
-const PageViewLogger = React.lazy(() => import('../PageViewLogger'));
-const Dashboard = React.lazy(() => import('../dashboard/Dashboard'));
+const Header = React.lazy(() => import("../Header"));
+const Footer = React.lazy(() => import("../Footer"));
+const Home = React.lazy(() => import("../Home"));
+const SignIn = React.lazy(() => import("../signin/SignIn"));
+const Register = React.lazy(() => import("../signup/Register"));
+const About = React.lazy(() => import("../About"));
+const GenericNotFound = React.lazy(() => import("../404"));
+const PageViewLogger = React.lazy(() => import("../PageViewLogger"));
+const Dashboard = React.lazy(() => import("../dashboard/Dashboard"));
 
 const useStyles = makeStyles((theme) => ({
   offset: theme.mixins.toolbar,
   mainContainer: {
     display: "flex",
-    minHeight: '100vh',
+    minHeight: "100vh",
     flexDirection: "column",
   },
 }));
 
-
-const preloadSDKs = firebaseApp => {
+const preloadSDKs = (firebaseApp) => {
   return Promise.all([
     preloadFirestore({
       firebaseApp,
@@ -49,27 +43,26 @@ const preloadSDKs = firebaseApp => {
         if (window.location.hostname === "localhost") {
           firestore().settings({
             host: "localhost:8080",
-            ssl: false
+            ssl: false,
           });
         }
         return firestore().enablePersistence();
-      }
+      },
     }),
-    preloadDatabase({ firebaseApp }),
     preloadStorage({
       firebaseApp,
       setup(storage) {
         return storage().setMaxUploadRetryTime(10000);
-      }
+      },
     }),
     preloadAuth({ firebaseApp }),
     preloadAnalytics({ firebaseApp }),
   ]);
 };
 
-const preloadData = async firebaseApp => {
+const preloadData = async (firebaseApp) => {
   const user = await preloadUser(firebaseApp);
-  if(user) console.log("user is auhed")
+  if (user) console.log("user is auhed");
 };
 
 function App() {
@@ -77,33 +70,30 @@ function App() {
   const classes = useStyles();
 
   preloadSDKs(firebaseApp).then(preloadData(firebaseApp));
-  
+
   return (
     <React.Fragment>
-      <SuspenseWithPerf fallback={<Loading />}
-        traceId={'load-header-status'}>
-          <Header />
-        </SuspenseWithPerf>
+      <SuspenseWithPerf fallback={<Loading />} traceId={"load-header-status"}>
+        <Header />
+      </SuspenseWithPerf>
       <div className={classes.offset} />
       <Container className={classes.mainContainer}>
-      <SuspenseWithPerf fallback={<Loading />}
-        traceId={'load-views-status'}>
-        <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/about" exact component={About} />
-              <Route path="/signin" exact component={SignIn} />
-              <Route path="/register" exact component={Register} />
-              <Route path="/dashboard" exact component={Dashboard} />
-              <Route path="*" exact={true} component={GenericNotFound} />
-        </Switch>
-        <PageViewLogger />
-      </SuspenseWithPerf>
+        <SuspenseWithPerf fallback={<Loading />} traceId={"load-views-status"}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" exact component={About} />
+            <Route path="/signin" exact component={SignIn} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="*" exact={true} component={GenericNotFound} />
+          </Switch>
+          <PageViewLogger />
+        </SuspenseWithPerf>
       </Container>
 
-      <SuspenseWithPerf fallback={<Loading />}
-        traceId={'load-footer-status'}>
-          <Footer />
-        </SuspenseWithPerf>
+      <SuspenseWithPerf fallback={<Loading />} traceId={"load-footer-status"}>
+        <Footer />
+      </SuspenseWithPerf>
     </React.Fragment>
   );
 }
