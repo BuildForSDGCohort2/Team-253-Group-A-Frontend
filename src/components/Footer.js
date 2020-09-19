286d79f9309f8d405e158a656b54622e69a43786
khammami <khammami@gmail.com>
2020-09-19 09:41:03 +0100

    Add link to "terms-of-services" in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import { grey } from '@material-ui/core/colors';
+import React from "react";
+import { Link as LinkRouter } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import { grey } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
   subTitle: {
     paddingLeft: theme.spacing(2),
   },
-  primary:{
+  primary: {
     color: grey[800],
   },
   copyright: {
@@ -27,7 +28,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 export default function Footer() {
   const classes = useStyles();
 
@@ -35,32 +35,45 @@ export default function Footer() {
     <React.Fragment>
       <div className={classes.root}>
         <Container maxWidth="md">
-
           <Grid container spacing={3}>
             <Grid item md={5} xs={12}>
               <Typography variant="h6" gutterBottom>
-              CleanOut
+                CleanOut
               </Typography>
             </Grid>
 
             <Grid item md={3} xs={6}>
-              <Typography variant="subtitle1" className={classes.subTitle} gutterBottom>
-              Legal
+              <Typography
+                variant="subtitle1"
+                className={classes.subTitle}
+                gutterBottom
+              >
+                Legal
               </Typography>
 
               <List component="nav" dense>
-                <ListItem button>
-                  <ListItemText className={classes.primary} primary="Terms of services"/>
+                <ListItem button component={LinkRouter} to="/terms-of-services">
+                  <ListItemText
+                    className={classes.primary}
+                    primary="Terms of services"
+                  />
                 </ListItem>
                 <ListItem button>
-                  <ListItemText className={classes.primary} primary="Privacy policy" />
+                  <ListItemText
+                    className={classes.primary}
+                    primary="Privacy policy"
+                  />
                 </ListItem>
               </List>
             </Grid>
 
             <Grid item md={2} xs={6}>
-              <Typography variant="subtitle1" className={classes.subTitle} gutterBottom>
-              Social
+              <Typography
+                variant="subtitle1"
+                className={classes.subTitle}
+                gutterBottom
+              >
+                Social
               </Typography>
 
               <List component="nav" dense>
@@ -68,7 +81,10 @@ export default function Footer() {
                   <ListItemText className={classes.primary} primary="Twitter" />
                 </ListItem>
                 <ListItem button>
-                  <ListItemText className={classes.primary} primary="Facebook" />
+                  <ListItemText
+                    className={classes.primary}
+                    primary="Facebook"
+                  />
                 </ListItem>
                 <ListItem button>
                   <ListItemText className={classes.primary} primary="Github" />
@@ -77,30 +93,46 @@ export default function Footer() {
             </Grid>
 
             <Grid item md={2} xs={6}>
-              <Typography variant="subtitle1" className={classes.subTitle} gutterBottom>
-              Project
+              <Typography
+                variant="subtitle1"
+                className={classes.subTitle}
+                gutterBottom
+              >
+                Project
               </Typography>
 
               <List component="nav" dense>
                 <ListItem button>
-                  <ListItemText className={classes.primary} primary="About us" />
+                  <ListItemText
+                    className={classes.primary}
+                    primary="About us"
+                  />
                 </ListItem>
                 <ListItem button>
-                  <ListItemText className={classes.primary} primary="Contact us" />
+                  <ListItemText
+                    className={classes.primary}
+                    primary="Contact us"
+                  />
                 </ListItem>
               </List>
             </Grid>
 
-            <Grid item xs={12}><Divider /></Grid>
-            
             <Grid item xs={12}>
-              <Typography variant="body1" className={classes.copyright} gutterBottom>
+              <Divider />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Typography
+                variant="body1"
+                className={classes.copyright}
+                gutterBottom
+              >
                 © CleanOut 2020. Made with love from Africa!
               </Typography>
             </Grid>
           </Grid>
         </Container>
       </div>
-  </React.Fragment>
+    </React.Fragment>
   );
 }
