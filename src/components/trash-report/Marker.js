65f848b83ce2b4692899e345e632529cb7eb691f
khammami <khammami@gmail.com>
2020-09-26 18:11:48 +0100

    Add marker component for Google maps

---
+++ src/components/trash-report/Marker.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Room from "@material-ui/icons/Room";
+import { red } from "@material-ui/core/colors";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    transform: "translateZ(0) translate(-50%, -100%)",
+    backfaceVisibility: "hidden",
+  },
+}));
+
+export default function Marker() {
+  const classes = useStyles();
+  return (
+    <Room className={classes.root} style={{ color: red[500], fontSize: 80 }} />
+  );
+}
