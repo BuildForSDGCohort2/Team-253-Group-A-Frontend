2d8477cd9bc84a735c7ea4a43862ddce4c7abb39
khammami <khammami@gmail.com>
2020-09-30 20:15:02 +0100

    Remove unused vars

--- src/components/trash-report/TrashReportCard.js
+++ src/components/trash-report/TrashReportCard.js
@@ -2,15 +2,15 @@ import React from "react";
 import { useStorage } from "reactfire";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
+/* import CardHeader from "@material-ui/core/CardHeader"; */
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import Avatar from "@material-ui/core/Avatar";
-
+/* import Avatar from "@material-ui/core/Avatar";
+ */
 const useStyles = makeStyles({
   root: {},
   media: {
