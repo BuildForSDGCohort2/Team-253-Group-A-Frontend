72ed8b158189159ce0aa9222e00acd50987c7156
ChinonsoIg <chinonso_ig@users.github.com>
2020-10-01 16:29:01 +0100

    contact form designed

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,19 +1,114 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
+import MailOutlineOutlinedIcon from "@material-ui/icons/MailOutlineOutlined";
+import SendRoundedIcon from "@material-ui/icons/SendRounded";
 
-const useStyles = makeStyles((theme) => ({
+const useStylesc = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
 }));
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(4),
+  },
+  roots: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: "25ch",
+  },
+  button: {
+    margin: theme.spacing(1),
+    fontSize: 20,
+    padding: theme.spacing(1),
+    backgroundColor: "#d6e4d6e6",
+    color: "#023e07",
+  },
+  icon: {
+    marginLeft: theme.spacing(1),
+    fontSize: 15,
+  },
+  message: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 80,
+    marginBottom: theme.spacing(5),
+    color: "#1a9224e3",
+  },
+}));
 
-export default function About() {
+export default function ContactUs() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-        about
+    <div>
+      <Typography gutterBottom variant="h3" style={{ fontWeight: "bold" }}>
+        Contact Us
+      </Typography>
+      <Paper className={classes.root}>
+        <MailOutlineOutlinedIcon className={classes.message} />
+        <TextField
+          id="standard-full-width"
+          style={{ margin: 8 }}
+          placeholder="Full name"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <TextField
+          id="standard-full-width"
+          style={{ margin: 8 }}
+          placeholder="Email address"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <TextField
+          id="standard-full-width"
+          style={{ margin: 8 }}
+          placeholder="Phone number"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <TextField
+          id="standard-full-width"
+          style={{ margin: 8 }}
+          placeholder="Leave a message"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          className={classes.button}
+        >
+          Send
+          <SendRoundedIcon className={classes.icon} />
+        </Button>
+      </Paper>
     </div>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import { Avatar } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -7,13 +10,64 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 export default function About() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-        about
+      <Grid container spacing={4}>
+        <Grid item xs={6} sm={3}>
+          <Avatar
+            src="https://avatars2.githubusercontent.com/u/46009285?s=460&v=4"
+            component="div"
+            className={classes.profilePicture}
+          >
+            OC
+          </Avatar>
+          <Typography
+            align="center"
+            gutterBottom
+            variant="subtitle1"
+            component="div"
+          >
+            Ojoechem Chinonso
+          </Typography>
+          <Typography
+            align="center"
+            gutterBottom
+            variant="caption"
+            component="div"
+          >
+            Nigeria
+          </Typography>
+        </Grid>
+
+        <Grid item xs={6} sm={3}>
+          <Avatar
+            src="https://avatars1.githubusercontent.com/u/297917?s=460&v=4"
+            component="div"
+            className={classes.profilePicture}
+          >
+            KH
+          </Avatar>
+          <Typography
+            align="center"
+            gutterBottom
+            variant="subtitle1"
+            component="div"
+          >
+            Khalil Hammami
+          </Typography>
+          <Typography
+            align="center"
+            gutterBottom
+            variant="caption"
+            component="div"
+          >
+            Tunisia
+          </Typography>
+        </Grid>
+      </Grid>
     </div>
   );
 }
