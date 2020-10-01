75d0b2149b38f0f447deace2f15ef0c6d7bbd0eb
khammami <khammami@gmail.com>
2020-10-02 00:45:38 +0100

    Define ui for report status component

--- src/components/trash-report/TrashReportStatus.js
+++ src/components/trash-report/TrashReportStatus.js
@@ -1,17 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
+import Chip from "@material-ui/core/Chip";
 
 const useStyles = makeStyles({
   root: {},
 });
 
 TrashReportStatus.propTypes = {
-  statusId: PropTypes.string,
+  statusId: PropTypes.string.isRequired,
 };
 
 export default function TrashReportStatus(props) {
   const classes = useStyles();
 
-  return <div className={classes.root}>{props.statusId}</div>;
+  return (
+    <div className={classes.root}>
+      <Chip
+        label={props.statusId == undefined ? "Open" : props.statusId}
+        variant="outlined"
+        color="secondary"
+      />
+    </div>
+  );
 }
