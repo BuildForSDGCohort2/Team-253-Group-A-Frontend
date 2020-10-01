0dcd02ef1015c2a2444aaf5bd7a3b16934959b95
khammami <khammami@gmail.com>
2020-10-01 12:51:06 +0100

    Test validate props in react (codacy)

--- src/components/trash-report/TrashReportStatus.js
+++ src/components/trash-report/TrashReportStatus.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   root: {},
 });
+
+TrashReportStatus.propTypes = {
+  statusId: PropTypes.string,
+};
 
 export default function TrashReportStatus(props) {
   const classes = useStyles();
