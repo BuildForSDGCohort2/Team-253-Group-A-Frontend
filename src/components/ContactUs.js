dbfa246444f38e7f891c43e5a168f5e57588b4fd
Khalil Hammami <khammami@gmail.com>
2020-10-02 01:04:19 +0100

    Remove unused vars in contact

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import Icon from "@material-ui/core/Icon";
 import StarRateTwoToneIcon from "@material-ui/icons/StarRateTwoTone";
 
 const useStyles = makeStyles((theme) => ({
