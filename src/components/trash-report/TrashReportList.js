fae5578b200ea9a7e0f72e56a9f22676ea9cb9de
khammami <khammami@gmail.com>
2020-10-03 22:36:53 +0100

    Show a message where no results for report list

--- src/components/trash-report/TrashReportList.js
+++ src/components/trash-report/TrashReportList.js
@@ -1,15 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useFirestore } from "reactfire";
+import { Link as LinkRouter } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-
 import TrashReportCard from "./TrashReportCard";
 import Loading from "../Loading";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import AddLocationOutlinedIcon from "@material-ui/icons/AddLocationOutlined";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    position: "relative",
+  },
+  noResultsContainer: {
+    display: "flex",
+    alignSelf: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    textAlign: "center",
+    margin: "auto",
+    "& > *": {
+      margin: theme.spacing(2),
+    },
+    width: "100%",
+    position: "absolute",
+    top: "50%",
+    transform: "translateY(-50%)",
   },
 }));
 
@@ -61,8 +80,8 @@ export default function TrashReportList(props) {
   return (
     <div className={classes.root}>
       {isLoading ? (
-        <Loading></Loading>
-      ) : (
+        <Loading />
+      ) : reportList.length > 0 ? (
         <Grid container spacing={2}>
           {reportList.map((report) => {
             return (
@@ -77,6 +96,23 @@ export default function TrashReportList(props) {
             );
           })}
         </Grid>
+      ) : (
+        <div className={classes.noResultsContainer}>
+          <Typography gutterBottom variant="body1" component="div">
+            No trash spots has been reported yet or they've been all cleaned.
+          </Typography>
+
+          <Button
+            component={LinkRouter}
+            to="/dashboard/spots/create"
+            startIcon={<AddLocationOutlinedIcon />}
+            variant="contained"
+            color="secondary"
+            size="large"
+          >
+            Report a spot of trash
+          </Button>
+        </div>
       )}
     </div>
   );
