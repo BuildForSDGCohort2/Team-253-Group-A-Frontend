41bf759b38a271455f071bfc840dd8746b416021
khammami <khammami@gmail.com>
2020-10-03 17:09:27 +0100

    Tweak report view design

--- src/components/trash-report/TrashReportView.js
+++ src/components/trash-report/TrashReportView.js
@@ -14,15 +14,18 @@ import TrashReportStatus from "./TrashReportStatus";
 import Avatar from "@material-ui/core/Avatar";
 import Moment from "react-moment";
 import Skeleton from "@material-ui/lab/Skeleton";
-
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import ImageOutlinedIcon from "@material-ui/icons/ImageOutlined";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Divider from "@material-ui/core/Divider";
 import GoogleMap from "google-map-react";
 import Marker from "./Marker";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {},
   media: {
     height: 0,
-    paddingTop: "56.25%",
+    paddingTop: "30%",
     backgroundColor: "#f7f7f7",
     backgroundRepeat: "no-repeat",
     backgroundSize: "contain",
@@ -31,10 +34,16 @@ const useStyles = makeStyles({
   cardTitle: {},
   reportDateFooter: { marginRight: "auto" },
   cardStatus: { marginLeft: "auto" },
-});
-
-/* let firstLoad = true; */
-let reportExists = true;
+  detailsTitle: {
+    margin: theme.spacing(2),
+  },
+  detailsContent: {
+    margin: theme.spacing(2),
+  },
+  imagesTitle: {
+    margin: "16px 32px",
+  },
+}));
 
 TrashReportView.propTypes = {
   reportId: PropTypes.string,
@@ -49,6 +58,7 @@ export default function TrashReportView(props) {
   const [data, setData] = React.useState(null);
   const [userProfile, setUserProfile] = React.useState(null);
   const [firstLoad, setFirstLoad] = React.useState(true);
+  const [reportExists, setReportExists] = React.useState(true);
 
   if (id === undefined) {
     if (props.reportId !== undefined && props.reportId.length > 0) {
@@ -81,101 +91,85 @@ export default function TrashReportView(props) {
             setLoading(false);
           });
       } else {
-        reportExists = false;
+        setReportExists(false);
       }
     });
   }
 
   if (reportExists) {
     return (
-      <Card className={classes.root}>
-        <CardHeader
-          avatar={
-            loading ? (
-              <Skeleton animation="wave" variant="circle">
-                <Avatar />
-              </Skeleton>
-            ) : (
-              <Link component={LinkRouter} to={`/profile/${userProfile.uid}`}>
-                <Avatar
-                  alt={userProfile.displayName}
-                  src={userProfile.photoURL}
-                  aria-label="profile picture"
-                />
-              </Link>
-            )
-          }
-          title={
-            loading ? (
-              <Skeleton
-                animation="wave"
-                height={10}
-                width="80%"
-                style={{ marginBottom: 6 }}
-              />
-            ) : (
-              <Link
-                component={LinkRouter}
-                color="secondary"
-                to={`/profile/${userProfile.uid})`}
-              >
-                {userProfile.displayName === undefined ||
-                userProfile.displayName == null
-                  ? "CleanOut user"
-                  : userProfile.displayName}
-              </Link>
-            )
-          }
-          subheader={
-            loading ? (
-              <Skeleton animation="wave" height={10} width="40%" />
-            ) : (
-              <Moment fromNow>{data.createdAt.toDate()}</Moment>
-            )
-          }
-          action={
-            loading ? (
-              <Skeleton animation="wave" height={10} width="40%" />
-            ) : (
-              <TrashReportStatus tagId={data.statusId} />
-            )
-          }
-        />
-
-        {loading ? (
-          <Skeleton animation="wave" variant="rect" className={classes.media} />
-        ) : (
-          <CardMedia>
-            <div
-              component="img"
-              className={classes.media}
-              style={{ backgroundImage: `url(${data.images[0].downloadUrl})` }}
-            />
-          </CardMedia>
-        )}
+      <Card className={classes.root} variant="outlined">
         <CardContent>
+          <CardHeader
+            avatar={
+              loading ? (
+                <Skeleton animation="wave" variant="circle">
+                  <Avatar />
+                </Skeleton>
+              ) : (
+                <Link component={LinkRouter} to={`/profile/${userProfile.uid}`}>
+                  <Avatar
+                    alt={userProfile.displayName}
+                    src={userProfile.photoURL}
+                    aria-label="profile picture"
+                  />
+                </Link>
+              )
+            }
+            title={
+              loading ? (
+                <Skeleton
+                  animation="wave"
+                  height={10}
+                  width="80%"
+                  style={{ marginBottom: 6 }}
+                />
+              ) : (
+                <Link
+                  component={LinkRouter}
+                  color="secondary"
+                  to={`/profile/${userProfile.uid})`}
+                >
+                  {userProfile.displayName === undefined ||
+                  userProfile.displayName == null
+                    ? "CleanOut user"
+                    : userProfile.displayName}
+                </Link>
+              )
+            }
+            subheader={
+              loading ? (
+                <Skeleton animation="wave" height={10} width="40%" />
+              ) : (
+                <Moment fromNow>{data.createdAt.toDate()}</Moment>
+              )
+            }
+            action={
+              loading ? (
+                <Skeleton animation="wave" height={10} width="40%" />
+              ) : (
+                <TrashReportStatus tagId={data.statusId} />
+              )
+            }
+          />
+
+          <Divider variant="middle" />
+
           {loading ? (
             <Skeleton
               animation="wave"
-              height={21}
-              style={{ marginBottom: 8 }}
-              className={classes.cardTitle}
+              height={41}
+              className={classes.detailsTitle}
             />
           ) : (
-            <Typography gutterBottom variant="h6" className={classes.cardTitle}>
-              {data.title}
-            </Typography>
+            <React.Fragment>
+              <Typography variant="h4" className={classes.detailsTitle}>
+                <LocationOnOutlinedIcon /> Spot Location
+              </Typography>
+            </React.Fragment>
           )}
 
-          {loading ? (
-            <Skeleton animation="wave" height={21} />
-          ) : (
-            <Typography gutterBottom variant="body2" paragraph>
-              {data.description}
-            </Typography>
-          )}
-
-          <div style={{ height: "350px", width: "100%" }}>
+          <div style={{ height: "250px", width: "100%" }}>
             <GoogleMap
               bootstrapURLKeys={{
                 key: process.env.REACT_APP_FIREBASE_APIKEY,
@@ -198,7 +192,73 @@ export default function TrashReportView(props) {
               )}
             </GoogleMap>
           </div>
+
+          {loading ? (
+            <Skeleton
+              animation="wave"
+              height={41}
+              className={classes.detailsTitle}
+            />
+          ) : (
+            <React.Fragment>
+              <Typography variant="h4" className={classes.detailsTitle}>
+                <InfoOutlinedIcon /> Spot Details
+              </Typography>
+              <Divider variant="middle" />
+            </React.Fragment>
+          )}
+
+          <div className={classes.detailsContent}>
+            {loading ? (
+              <Skeleton
+                animation="wave"
+                height={21}
+                style={{ marginBottom: 8 }}
+                className={classes.cardTitle}
+              />
+            ) : (
+              <Typography
+                gutterBottom
+                variant="h6"
+                className={classes.cardTitle}
+              >
+                {data.title}
+              </Typography>
+            )}
+
+            {loading ? (
+              <Skeleton animation="wave" height={21} />
+            ) : (
+              <Typography gutterBottom variant="body2" paragraph>
+                {data.description}
+              </Typography>
+            )}
+          </div>
         </CardContent>
+
+        {loading ? (
+          <Skeleton
+            animation="wave"
+            height={41}
+            className={classes.imagesTitle}
+          />
+        ) : (
+          <Typography variant="h4" className={classes.imagesTitle}>
+            <ImageOutlinedIcon /> Spot Images
+          </Typography>
+        )}
+
+        {loading ? (
+          <Skeleton animation="wave" variant="rect" className={classes.media} />
+        ) : (
+          <CardMedia>
+            <div
+              component="img"
+              className={classes.media}
+              style={{ backgroundImage: `url(${data.images[0].downloadUrl})` }}
+            />
+          </CardMedia>
+        )}
       </Card>
     );
   } else {
