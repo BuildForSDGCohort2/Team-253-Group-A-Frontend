632e1ffd9f2e793f226c69ca5144537b53d49fb6
khammami <khammami@gmail.com>
2020-10-05 16:28:08 +0100

    Relook contact us

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,121 +1,104 @@
 import React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import StarRateTwoToneIcon from "@material-ui/icons/StarRateTwoTone";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing(4),
   },
-  textFieldLeft: {
-    marginLeft: theme.spacing(0),
-    marginRight: "3%",
-    width: "47%",
-  },
-  textFieldRight: {
-    marginLeft: "3%",
-    marginRight: theme.spacing(0),
-    width: "47%",
-  },
-  button: {
-    margin: theme.spacing(1),
-    fontSize: 20,
-    padding: theme.spacing(1),
-    // backgroundColor: "#d6e4d6e6",
-    backgroundColor: "#023e07",
-    color: "white",
-  },
-  icon: {
-    marginLeft: 2,
-    marginBottom: 4,
-    fontSize: 10,
-    color: "red",
-  },
-  message: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: 80,
-    marginBottom: theme.spacing(5),
-    color: "#1a9224e3",
-  },
 }));
 
-export default function ContactUs() {
+export default function ContactUs(props) {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Typography
-        gutterBottom
-        variant="h3"
-        style={{ fontWeight: "bold", color: "#023e07" }}
-      >
-        Get In Touch
-      </Typography>
+    <div {...props} className={classes.root}>
       <form noValidate autoComplete="off">
-        <Typography>
-          Name
-          <StarRateTwoToneIcon className={classes.icon} />
-        </Typography>
-        <TextField
-          id="outlined-basic"
-          variant="outlined"
-          margin="normal"
-          className={classes.textFieldLeft}
-          style={{
-            backgroundColor: "white",
-          }}
-        />
-        <TextField
-          id="outlined-basic"
-          variant="outlined"
-          margin="normal"
-          className={classes.textFieldRight}
-          style={{
-            backgroundColor: "white",
-          }}
-        />
-        <Typography>
-          Email
-          <StarRateTwoToneIcon className={classes.icon} />
-        </Typography>
-        <TextField
-          id="outlined-full-width-static"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          style={{
-            backgroundColor: "white",
-          }}
-        />
-        <Typography>
-          Comment or Message
-          <StarRateTwoToneIcon className={classes.icon} />
-        </Typography>
-        <TextField
-          id="outlined-multiline-static"
-          fullWidth
-          multiline
-          rows={6}
-          variant="outlined"
-          margin="normal"
-          style={{
-            backgroundColor: "white",
-          }}
-        />
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <Typography gutterBottom variant="h5">
+              Get In Touch
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper elevation={0}>
+              <TextField
+                variant="outlined"
+                type="text"
+                required
+                name="fullName"
+                fullWidth
+                label="Your Name"
+                color="secondary"
+                /* onChange={(e) => setLastName(e.target.value)} */
+              />
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper elevation={0}>
+              <TextField
+                variant="outlined"
+                type="email"
+                required
+                name="email"
+                fullWidth
+                label="Your email"
+                color="secondary"
+                /* onChange={(e) => setLastName(e.target.value)} */
+              />
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper elevation={0}>
+              <TextField
+                variant="outlined"
+                type="text"
+                required
+                name="subject"
+                fullWidth
+                label="Subject"
+                color="secondary"
+                /* onChange={(e) => setLastName(e.target.value)} */
+              />
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper elevation={0}>
+              <TextField
+                variant="outlined"
+                type="text"
+                required
+                name="email"
+                fullWidth
+                label="Your message"
+                color="secondary"
+                multiline
+                rows={5}
+                /* onChange={(e) => setLastName(e.target.value)} */
+              />
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <ReCAPTCHA sitekey={process.env.REACT_APP_RECAPTCHA_CLIENT_KEY} />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              className={classes.button}
+            >
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
       </form>
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        className={classes.button}
-      >
-        Submit
-      </Button>
     </div>
   );
 }
